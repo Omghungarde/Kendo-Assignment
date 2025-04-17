104940e085c141950204101d9bf35e0dc8c831c5
omwaii <om.ghungarde@waiin.com>
2025-04-17 18:44:33 +0530

    feat(ui): implement dark mode toggle and update styles for improved UI experience

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,5 +13,16 @@ export class NavbarComponent {
   // toggleNavbar() {
   //   this.navbarOpen = !this.navbarOpen;
   // }
-  
+  isDarkMode = false;
+
+toggleDarkMode() {
+  this.isDarkMode = !this.isDarkMode;
+
+  if (this.isDarkMode) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+}
+
 }
